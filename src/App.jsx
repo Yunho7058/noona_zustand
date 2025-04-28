0b430b92644b72b12bd58db4e63d0bbce023ee14
Yunho <yunho@Yunhoui-MacBookAir.local>
2025-04-28 23:36:39 +0900

    omz test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 // https://mui.com/material-ui/getting-started/installation/
 import "./App.css";
+import CountTest from "./page/CountTest";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <CountTest />
+    </>
+  );
 }
 
 export default App;
